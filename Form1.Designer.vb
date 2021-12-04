ea7f2d84539c861839627de58e6b634ac3f300d7
SigmaDolphin <49135857+SigmaDolphin@users.noreply.github.com>
2021-12-04 08:17:44 -0600

    Initial commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,246 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.LoadButton = New System.Windows.Forms.Button()
+        Me.WriteButton = New System.Windows.Forms.Button()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ReadButton = New System.Windows.Forms.Button()
+        Me.SaveButton = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'LoadButton
+        '
+        Me.LoadButton.Location = New System.Drawing.Point(12, 12)
+        Me.LoadButton.Name = "LoadButton"
+        Me.LoadButton.Size = New System.Drawing.Size(75, 23)
+        Me.LoadButton.TabIndex = 0
+        Me.LoadButton.Text = "Load File"
+        Me.LoadButton.UseVisualStyleBackColor = True
+        '
+        'WriteButton
+        '
+        Me.WriteButton.Location = New System.Drawing.Point(6, 58)
+        Me.WriteButton.Name = "WriteButton"
+        Me.WriteButton.Size = New System.Drawing.Size(92, 23)
+        Me.WriteButton.TabIndex = 1
+        Me.WriteButton.Text = "Write"
+        Me.WriteButton.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(110, 20)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(92, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Change Identity"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 113)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(173, 21)
+        Me.ComboBox1.TabIndex = 6
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 149)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(240, 320)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(258, 429)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(88, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Next"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(258, 455)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Index:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(300, 455)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Sprite"
+        '
+        'ReadButton
+        '
+        Me.ReadButton.Location = New System.Drawing.Point(6, 20)
+        Me.ReadButton.Name = "ReadButton"
+        Me.ReadButton.Size = New System.Drawing.Size(92, 23)
+        Me.ReadButton.TabIndex = 11
+        Me.ReadButton.Text = "Read"
+        Me.ReadButton.UseVisualStyleBackColor = True
+        '
+        'SaveButton
+        '
+        Me.SaveButton.Location = New System.Drawing.Point(12, 54)
+        Me.SaveButton.Name = "SaveButton"
+        Me.SaveButton.Size = New System.Drawing.Size(75, 23)
+        Me.SaveButton.TabIndex = 12
+        Me.SaveButton.Text = "Save File"
+        Me.SaveButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.ReadButton)
+        Me.GroupBox1.Controls.Add(Me.WriteButton)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Location = New System.Drawing.Point(191, 5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(208, 138)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Chip Actions"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "COM Port:"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(6, 108)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox2.TabIndex = 12
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(258, 269)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(88, 23)
+        Me.Button7.TabIndex = 14
+        Me.Button7.Text = "Save Current"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(258, 380)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(88, 23)
+        Me.Button8.TabIndex = 15
+        Me.Button8.Text = "Load Sheet"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(258, 341)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(88, 23)
+        Me.Button1.TabIndex = 16
+        Me.Button1.Text = "Save Sheet"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(411, 485)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.SaveButton)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.LoadButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Legendz Tool"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents LoadButton As System.Windows.Forms.Button
+    Friend WithEvents WriteButton As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents ReadButton As System.Windows.Forms.Button
+    Friend WithEvents SaveButton As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,278 @@
+﻿Public Class Form1
+    Dim Legendz(1023) As Byte
+    Dim singleByte(0) As Byte
+    Dim filePath As String
+    Dim offset As Integer
+    Dim spriteIndex As Integer
+    Dim oDialog As New OpenFileDialog
+    Dim sDialog As New SaveFileDialog
+    Dim sprite As New Bitmap(24, 32)
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LegendzDB()
+        spriteIndex = 0
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComboBox2.Items.Add(sp)
+        Next
+    End Sub
+
+    'Load File
+    Private Sub LoadButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LoadButton.Click
+        oDialog.Filter = "Binary files (*.bin)|*.bin|All files (*.*)|*.*"
+        oDialog.ShowDialog()
+        If oDialog.FileName = "" Then
+            Exit Sub
+        End If
+        filePath = oDialog.FileName
+        oDialog.FileName = ""
+        Legendz = System.IO.File.ReadAllBytes(filePath)
+        ComboBox1.SelectedIndex = Legendz(770)
+        Label2.Text = 0
+        traceSprite(0)
+    End Sub
+
+    'Save File
+    Private Sub SaveButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles SaveButton.Click
+        sDialog.DefaultExt() = ".bin"
+        sDialog.Filter = "Binary files (*.bin)|*.bin|All files (*.*)|*.*"
+        sDialog.ShowDialog()
+        If sDialog.FileName = "" Then
+            Exit Sub
+        End If
+        filePath = sDialog.FileName
+        sDialog.FileName = ""
+        System.IO.File.WriteAllBytes(filePath, Legendz)
+    End Sub
+
+    'Write to Chip
+    Private Sub WriteButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles WriteButton.Click
+        offset = 0
+        SerialPort1.PortName = ComboBox2.SelectedItem
+        SerialPort1.Open()
+        SerialPort1.Write("W")
+        Do
+            System.Threading.Thread.Sleep(20)
+            SerialPort1.Write(Legendz, offset, 1)
+            System.Threading.Thread.Sleep(20)
+            offset = offset + 1
+            Application.DoEvents()
+        Loop While offset < 1024
+        SerialPort1.Close()
+        MsgBox("Write Complete")
+    End Sub
+
+    'Read from Chip
+    Private Sub ReadButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ReadButton.Click
+        offset = 0
+        SerialPort1.PortName = ComboBox2.SelectedItem
+        SerialPort1.Open()
+        SerialPort1.Write("R")
+        Do
+            Legendz(offset) = SerialPort1.ReadByte()
+            offset = offset + 1
+            Application.DoEvents()
+        Loop While offset < 1024
+        SerialPort1.Close()
+        ComboBox1.SelectedIndex = Legendz(770)
+        Label2.Text = 0
+        traceSprite(0)
+    End Sub
+
+    'Change ID in Chip
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        singleByte(0) = ComboBox1.SelectedIndex
+        SerialPort1.PortName = ComboBox2.SelectedItem
+        SerialPort1.Open()
+        SerialPort1.Write("1")
+        SerialPort1.Write(singleByte, 0, 1)
+        SerialPort1.Close()
+    End Sub
+
+
+    Sub traceSprite(ByVal index As Integer)
+        Dim x As Integer
+        Dim y As Integer
+        Dim spriteLoop As Integer
+        Dim strBuffer As String
+        Dim strBuffer2 As String
+        Dim strBuffer3 As String
+        Dim binaryString As String
+        sprite = New Bitmap(24, 32)
+        PictureBox1.Image = Nothing
+
+        spriteLoop = 96 * index
+        y = 0
+
+        Do While y <= 31
+
+            strBuffer = "00000000" & Convert.ToString(Legendz(spriteLoop), 2)
+            strBuffer = strBuffer.Substring(strBuffer.Length - 8)
+            strBuffer2 = "00000000" & Convert.ToString(Legendz(spriteLoop + 1), 2)
+            strBuffer2 = strBuffer2.Substring(strBuffer2.Length - 8)
+            strBuffer3 = "00000000" & Convert.ToString(Legendz(spriteLoop + 2), 2)
+            strBuffer3 = strBuffer3.Substring(strBuffer3.Length - 8)
+            binaryString = strBuffer & strBuffer2 & strBuffer3
+
+            x = 0
+            Dim a
+            For Each a In binaryString
+                If a = "1" Then
+                    sprite.SetPixel(x, y, Color.Black)
+                Else
+                    sprite.SetPixel(x, y, Color.White)
+                End If
+                x = x + 1
+            Next
+            spriteLoop = spriteLoop + 3
+            y = y + 1
+        Loop
+        PictureBox1.Image = sprite
+        PictureBox1.Refresh()
+    End Sub
+
+    Private Sub PictureBox1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles PictureBox1.Paint
+        e.Graphics.ScaleTransform(10, 10)
+        e.Graphics.PixelOffsetMode = System.Drawing.Drawing2D.PixelOffsetMode.Half
+        e.Graphics.InterpolationMode = Drawing2D.InterpolationMode.NearestNeighbor
+        e.Graphics.DrawImage(sprite, 0, 0)
+    End Sub
+
+    'Next sprite
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Label2.Text = Label2.Text + 1
+        If Label2.Text = 8 Then
+            Label2.Text = 0
+        End If
+        spriteIndex = Label2.Text
+        traceSprite(spriteIndex)
+    End Sub
+
+    'Save selected sprite
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        sDialog.DefaultExt() = ".png"
+        sDialog.Filter = "PNG files (*.png)|*.png|All files (*.*)|*.*"
+        sDialog.ShowDialog()
+        If sDialog.FileName = "" Then
+            Exit Sub
+        End If
+        filePath = sDialog.FileName
+        sDialog.FileName = ""
+        PictureBox1.Image.Save(filePath, Imaging.ImageFormat.Png)
+    End Sub
+
+    'Save Sprite sheet
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sDialog.DefaultExt() = ".png"
+        sDialog.Filter = "PNG files (*.png)|*.png|All files (*.*)|*.*"
+        sDialog.ShowDialog()
+        If sDialog.FileName = "" Then
+            Exit Sub
+        End If
+        filePath = sDialog.FileName
+        sDialog.FileName = ""
+        drawSheet.Save(filePath, Imaging.ImageFormat.Png)
+    End Sub
+
+    Function drawSheet() As Image
+        Dim spriteSheet As Bitmap
+        Dim x As Integer
+        Dim y As Integer
+        Dim spriteLoop As Integer = 0
+        Dim strBuffer As String
+        Dim strBuffer2 As String
+        Dim strBuffer3 As String
+        Dim binaryString As String
+
+        Dim spriteCount As Integer
+        Dim rowCount As Integer
+
+
+        spriteSheet = New Bitmap(96, 64)
+
+        For rowCount = 0 To 1
+            For spriteCount = 0 To 3
+                y = 32 * rowCount
+                Do While y <= (32 * (rowCount + 1)) - 1
+
+                    strBuffer = "00000000" & Convert.ToString(Legendz(spriteLoop), 2)
+                    strBuffer = strBuffer.Substring(strBuffer.Length - 8)
+                    strBuffer2 = "00000000" & Convert.ToString(Legendz(spriteLoop + 1), 2)
+                    strBuffer2 = strBuffer2.Substring(strBuffer2.Length - 8)
+                    strBuffer3 = "00000000" & Convert.ToString(Legendz(spriteLoop + 2), 2)
+                    strBuffer3 = strBuffer3.Substring(strBuffer3.Length - 8)
+                    binaryString = strBuffer & strBuffer2 & strBuffer3
+
+                    x = 24 * spriteCount
+                    Dim a
+                    For Each a In binaryString
+                        If a = "1" Then
+                            spriteSheet.SetPixel(x, y, Color.Black)
+                        Else
+                            spriteSheet.SetPixel(x, y, Color.White)
+                        End If
+                        x += 1
+                    Next
+                    spriteLoop += 3
+                    y += 1
+                Loop
+            Next
+        Next
+        Return spriteSheet
+    End Function
+
+    'Load sprite sheet
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        oDialog.Filter = "PNG files (*.png)|*.png|All files (*.*)|*.*"
+        oDialog.ShowDialog()
+        If oDialog.FileName = "" Then
+            Exit Sub
+        End If
+        filePath = oDialog.FileName
+        oDialog.FileName = ""
+        readSheet()
+        Label2.Text = 0
+        traceSprite(0)
+    End Sub
+
+    Sub readSheet()
+        Dim spriteSheet As New Bitmap(filePath)
+        Dim x As Integer
+        Dim y As Integer
+        Dim spriteLoop As Integer = 0
+
+        Dim byteBuffer As Byte
+        Dim spriteCount As Integer
+        Dim rowCount As Integer
+        Dim sectionCount As Integer
+
+        If spriteSheet.Width <> 96 Or spriteSheet.Height <> 64 Then
+            MsgBox("Wrong image size, image needs to be 96x64")
+            Exit Sub
+        End If
+
+        For rowCount = 0 To 1
+            For spriteCount = 0 To 3
+                y = 32 * rowCount
+
+                Do While y <= (32 * (rowCount + 1)) - 1
+                    x = 24 * spriteCount
+                    For sectionCount = 0 To 2
+                        For b = 0 To 7
+                            If spriteSheet.GetPixel(b + (8 * sectionCount) + x, y).R <> 255 Then
+                                byteBuffer += 1
+                            End If
+                            If b <> 7 Then
+                                byteBuffer <<= 1
+                            End If
+                        Next
+                        Legendz(spriteLoop) = byteBuffer
+                        spriteLoop += 1
+                        byteBuffer = 0
+                    Next
+
+                    y += 1
+                Loop
+            Next
+        Next
+    End Sub
+End Class
