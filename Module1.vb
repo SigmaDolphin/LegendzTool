e744b312d3c7cb8fc28f698c3483ea8178cb7db9
SigmaDolphin <49135857+SigmaDolphin@users.noreply.github.com>
2022-03-19 14:27:00 -0600

    Overhaul to external Database
    Complete overhaul of the list system to a more proper external XML based database

--- Module1.vb
+++ Module1.vb
@@ -1,123 +1,104 @@
-﻿Module Module1
+Imports System.Xml
+Module Module1
 
-    Sub LegendzDB()
-        Form1.ComboBox1.Items.Insert(0, "--(0)")
-        Form1.ComboBox1.Items.Insert(1, "Windragon")
-        Form1.ComboBox1.Items.Insert(2, "Blazedragon")
-        Form1.ComboBox1.Items.Insert(3, "Windragon")
-        Form1.ComboBox1.Items.Insert(4, "Mermaid")
-        Form1.ComboBox1.Items.Insert(5, "Werewolf")
-        Form1.ComboBox1.Items.Insert(6, "Will-o-Wisp")
-        Form1.ComboBox1.Items.Insert(7, "Tornado KD")
-        Form1.ComboBox1.Items.Insert(8, "Blazedragon")
-        Form1.ComboBox1.Items.Insert(9, "Goblin")
-        Form1.ComboBox1.Items.Insert(10, "Yeti")
-        Form1.ComboBox1.Items.Insert(11, "Hellhound")
-        Form1.ComboBox1.Items.Insert(12, "Volcano KD")
-        Form1.ComboBox1.Items.Insert(13, "WD Berserk")
-        Form1.ComboBox1.Items.Insert(14, "BD Berserk")
-        Form1.ComboBox1.Items.Insert(15, "Fire Giant")
-        Form1.ComboBox1.Items.Insert(16, "Undine")
-        Form1.ComboBox1.Items.Insert(17, "Peryton")
-        Form1.ComboBox1.Items.Insert(18, "Troll")
-        Form1.ComboBox1.Items.Insert(19, "Commander WD")
-        Form1.ComboBox1.Items.Insert(20, "Manticore")
-        Form1.ComboBox1.Items.Insert(21, "Carbuncle")
-        Form1.ComboBox1.Items.Insert(22, "Commander BD")
-        Form1.ComboBox1.Items.Insert(23, "Dwarf")
-        Form1.ComboBox1.Items.Insert(24, "Skeleton")
-        Form1.ComboBox1.Items.Insert(25, "Earthquake KD")
-        Form1.ComboBox1.Items.Insert(26, "Storm KD")
-        Form1.ComboBox1.Items.Insert(27, "Wyvern")
-        Form1.ComboBox1.Items.Insert(28, "--(28)")
-        Form1.ComboBox1.Items.Insert(29, "--(29)")
-        Form1.ComboBox1.Items.Insert(30, "Ranshiin")
-        Form1.ComboBox1.Items.Insert(31, "Ranshiin")
-        Form1.ComboBox1.Items.Insert(32, "Chibi Shiron")
-        Form1.ComboBox1.Items.Insert(33, "Commander WD.B")
-        Form1.ComboBox1.Items.Insert(34, "Commander BD.V")
-        Form1.ComboBox1.Items.Insert(35, "Chimera")
-        Form1.ComboBox1.Items.Insert(36, "Triton")
-        Form1.ComboBox1.Items.Insert(37, "Gargoyle")
-        Form1.ComboBox1.Items.Insert(38, "Tornado Giant")
-        Form1.ComboBox1.Items.Insert(39, "Commander WD")
-        Form1.ComboBox1.Items.Insert(40, "Commander BD")
-        Form1.ComboBox1.Items.Insert(41, "Gryphon")
-        Form1.ComboBox1.Items.Insert(42, "Big Foot")
-        Form1.ComboBox1.Items.Insert(43, "Spiritual WD")
-        Form1.ComboBox1.Items.Insert(44, "Hexadragon")
-        Form1.ComboBox1.Items.Insert(45, "WD Berserker")
-        Form1.ComboBox1.Items.Insert(46, "BD Berserker")
-        Form1.ComboBox1.Items.Insert(47, "Colonel WD")
-        Form1.ComboBox1.Items.Insert(48, "Colonel Blazedragon")
-        Form1.ComboBox1.Items.Insert(49, "Devouring Crocodile (B)")
-        Form1.ComboBox1.Items.Insert(50, "Harpy")
-        Form1.ComboBox1.Items.Insert(51, "Cait Sith")
-        Form1.ComboBox1.Items.Insert(52, "Commander BD.V")
-        Form1.ComboBox1.Items.Insert(53, "Devouring Crocodile")
-        Form1.ComboBox1.Items.Insert(54, "Vampire")
-        Form1.ComboBox1.Items.Insert(55, "Spiritual KD")
-        Form1.ComboBox1.Items.Insert(56, "Orc")
-        Form1.ComboBox1.Items.Insert(57, "Spriggan")
-        Form1.ComboBox1.Items.Insert(58, "Commander WD.B")
-        Form1.ComboBox1.Items.Insert(59, "Bicorn")
-        Form1.ComboBox1.Items.Insert(60, "Centaur")
-        Form1.ComboBox1.Items.Insert(61, "Nekrom KD")
-        Form1.ComboBox1.Items.Insert(62, "Golem")
-        Form1.ComboBox1.Items.Insert(63, "Ifrit")
-        Form1.ComboBox1.Items.Insert(64, "Ranshiin")
-        Form1.ComboBox1.Items.Insert(65, "Gorgon")
-        Form1.ComboBox1.Items.Insert(66, "Kraken")
-        Form1.ComboBox1.Items.Insert(67, "Minotaur")
-        Form1.ComboBox1.Items.Insert(68, "Oberon")
-        Form1.ComboBox1.Items.Insert(69, "Lich")
-        Form1.ComboBox1.Items.Insert(70, "--(70)")
-        Form1.ComboBox1.Items.Insert(71, "--(71)")
-        Form1.ComboBox1.Items.Insert(72, "Colonel WD.T")
-        Form1.ComboBox1.Items.Insert(73, "Phoenix")
-        Form1.ComboBox1.Items.Insert(74, "Behemoth")
-        Form1.ComboBox1.Items.Insert(75, "Jabberwock")
-        Form1.ComboBox1.Items.Insert(76, "Leviathan")
-        Form1.ComboBox1.Items.Insert(77, "Fenrir")
-        Form1.ComboBox1.Items.Insert(78, "Colonel BD.V")
-        Form1.ComboBox1.Items.Insert(79, "Sphinx")
-        Form1.ComboBox1.Items.Insert(80, "Cyclops")
-        Form1.ComboBox1.Items.Insert(81, "--(81)")
-        Form1.ComboBox1.Items.Insert(82, "--(82)")
-        Form1.ComboBox1.Items.Insert(83, "Jabberwock")
-        Form1.ComboBox1.Items.Insert(84, "Scylla")
-        Form1.ComboBox1.Items.Insert(85, "Titan")
-        Form1.ComboBox1.Items.Insert(86, "Echidna")
-        Form1.ComboBox1.Items.Insert(87, "--(87)")
-        Form1.ComboBox1.Items.Insert(88, "Colonel Blazedragon")
-        Form1.ComboBox1.Items.Insert(89, "Ranshiin")
-        Form1.ComboBox1.Items.Insert(90, "Chibi Shiron")
-        Form1.ComboBox1.Items.Insert(91, "Blazedragon")
-        Form1.ComboBox1.Items.Insert(92, "Manga Windragon")
-        Form1.ComboBox1.Items.Insert(93, "Gryphon")
-        Form1.ComboBox1.Items.Insert(94, "Big Foot")
-        Form1.ComboBox1.Items.Insert(95, "Tiamat")
-        Form1.ComboBox1.Items.Insert(96, "Windragon")
-        Form1.ComboBox1.Items.Insert(97, "Colonel Blazedragon")
-        Form1.ComboBox1.Items.Insert(98, "Fire Giant")
-        Form1.ComboBox1.Items.Insert(99, "Undine")
-        Form1.ComboBox1.Items.Insert(100, "Peryton")
-        Form1.ComboBox1.Items.Insert(101, "Troll")
-        Form1.ComboBox1.Items.Insert(102, "Wyvern")
-        Form1.ComboBox1.Items.Insert(103, "Storm KD")
-        Form1.ComboBox1.Items.Insert(104, "Carbuncle")
-        Form1.ComboBox1.Items.Insert(105, "--(105")
-        Form1.ComboBox1.Items.Insert(106, "--(106)")
-        Form1.ComboBox1.Items.Insert(107, "--(107)")
-        Form1.ComboBox1.Items.Insert(108, "--(108)")
-        Form1.ComboBox1.Items.Insert(109, "Giant Crab")
-        Form1.ComboBox1.Items.Insert(110, "--(110)")
-        Form1.ComboBox1.Items.Insert(111, "--(111)")
-        Form1.ComboBox1.Items.Insert(112, "--(112)")
-        Form1.ComboBox1.Items.Insert(113, "Hydra")
-        Form1.ComboBox1.Items.Insert(114, "Iron Golem")
-        Form1.ComboBox1.Items.Insert(115, "Valkyrie")
-        Form1.ComboBox1.Items.Insert(116, "Cerberus")
+    Sub LegendzIndexes(ByVal exepath As String)
+        Dim count As Integer
+        Dim xmlFile As XmlReader
+        xmlFile = XmlReader.Create(exepath & "LegendzDB.xml")
+        Dim ds As New DataSet
+        Dim dv As DataView
+        ds.ReadXml(xmlFile)
+
+        'Load Legendz IDs
+        dv = New DataView(ds.Tables(0))
+        dv.Sort = "index"
+
+        count = 0
+        Do
+            Dim index As Integer = dv.Find(count)
+            If index = -1 Then
+                Exit Do
+            Else
+                Form1.ComboBox1.Items.Insert(count, dv(index)("name").ToString())
+            End If
+            count = count + 1
+        Loop
+
+        'Load Elements
+        dv = New DataView(ds.Tables(1))
+        dv.Sort = "index"
+
+        count = 0
+        Do
+            Dim index As Integer = dv.Find(count)
+            If index = -1 Then
+                Exit Do
+            Else
+                Form1.ElementC.Items.Insert(count, dv(index)("name").ToString())
+            End If
+            count = count + 1
+        Loop
+
+        'Load Ranks
+        dv = New DataView(ds.Tables(2))
+        dv.Sort = "index"
+
+        count = 0
+        Do
+            Dim index As Integer = dv.Find(count)
+            If index = -1 Then
+                Exit Do
+            Else
+                Form1.RankC.Items.Insert(count, dv(index)("name").ToString())
+            End If
+            count = count + 1
+        Loop
+
+        'Load Temperatures
+        dv = New DataView(ds.Tables(3))
+        dv.Sort = "index"
+
+        count = 0
+        Do
+            Dim index As Integer = dv.Find(count)
+            If index = -1 Then
+                Exit Do
+            Else
+                Form1.TemperatureC.Items.Insert(count, dv(index)("name").ToString())
+            End If
+            count = count + 1
+        Loop
+
+        'Load Humidity
+        dv = New DataView(ds.Tables(4))
+        dv.Sort = "index"
+
+        count = 0
+        Do
+            Dim index As Integer = dv.Find(count)
+            If index = -1 Then
+                Exit Do
+            Else
+                Form1.HumidityC.Items.Insert(count, dv(index)("name").ToString())
+            End If
+            count = count + 1
+        Loop
+
+        'Load Move Names
+        dv = New DataView(ds.Tables(5))
+        dv.Sort = "index"
+
+        count = 0
+        Do
+            Dim index As Integer = dv.Find(count)
+            If index = -1 Then
+                Exit Do
+            Else
+                Form1.ShiftE1.Items.Insert(count, dv(index)("name").ToString())
+                Form1.ShiftE2.Items.Insert(count, dv(index)("name").ToString())
+                Form1.ShiftE3.Items.Insert(count, dv(index)("name").ToString())
+            End If
+            count = count + 1
+        Loop
     End Sub
-
 End Module
